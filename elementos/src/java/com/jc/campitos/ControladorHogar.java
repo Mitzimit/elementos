6e61684feb7a834e7eaa50df55558059df1feae4
campitos <campitos@campitoslap2>
2014-01-21 18:59:02 -0600

    Primera actualizacion

---
+++ elementos/src/java/com/jc/campitos/ControladorHogar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jc.campitos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author campitos
+ */
+@Controller
+public class ControladorHogar {
+    
+@RequestMapping("/")
+    public String hola(){
+        
+        return "index";
+    }   
+
+}
